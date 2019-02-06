3595d153e052c8d87f605944deb633b8ab247647
Jake Atkin <jakeatkin@Jakes-MacBook-Air.local>
2019-02-06 14:45:58 +0000

    Getting started off colins base

--- spec/AirportSpec.js
+++ spec/AirportSpec.js
@@ -1,12 +1,17 @@
 describe("Airport", function() {
 
     var airport = new Airport();
+
     var plane = jasmine.createSpyObj('plane',['land']);
+// createSpyObj - basically a double that accepts that its an instance of it
 
     describe('it instructs a plane to land', function() {
         it('sends a land command', function(){
             airport.land(plane);
             expect(plane.land).toHaveBeenCalled();
+            // Checks that the method of plane.land has actually been called.
         });
     });
 });
+
+//  sdasdsad
--- spec/PlaneSpec.js
+++ spec/PlaneSpec.js
@@ -1,3 +1,5 @@
 describe("Plane", function() {
 
 });
+
+// zdsadsad
